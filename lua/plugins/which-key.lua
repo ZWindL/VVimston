cea8d743c1289ad2a914ca4569023f94c43b9ec8
ZWindL <zwindl@hotmail.com>
2024-07-15 22:04:17 +0800

    update which-key config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -5,21 +5,20 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
+        replace = {
+            key = {
+                { "<space>", "SPC" },
+                { "<cr>", "RET" },
+                { "<tab>", "TAB" },
+            },
+        },
         icons = {
             breadcrumb = icons.arrows.doubleArrowRight, -- symbol used in the command line area that shows your active key combo
             separator = icons.arrows.smallArrowRight, -- symbol used between a key and it's label
             group = icons.ui.plus, -- symbol prepended to a group
         },
-        window = {
+        win = {
             border = "none", -- none, single, double, shadow
-            position = "bottom", -- bottom, top
-            margin = { 0, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-            padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-        },
-        key_labels = {
-            ["<space>"] = "SPC",
-            ["<cr>"] = "RET",
-            ["<tab>"] = "TAB",
         },
         layout = {
             height = { min = 3, max = 25 }, -- min and max height of the columns
@@ -36,23 +35,19 @@ return {
             z = true, -- bindings for folds, spelling and others prefixed with z
             g = true, -- bindings for prefixed with g
         },
-        groups = {
+        spec = {
             mode = { "n", "v" },
-            ["<leader>b"] = { name = "Buffers" },
-            ["<leader>f"] = { name = "Files" },
-            ["<leader>l"] = { name = "LSP" },
-            ["<leader>m"] = { name = "Misc" },
-            ["<leader>q"] = { name = "Quickfix" },
-            ["<leader>R"] = { name = "Replace" },
-            ["<leader>mS"] = { name = "TreeSJ" },
-            ["<leader>s"] = { name = "Search" },
-            ["<leader>t"] = { name = "Toggles" },
-            ["<leader>z"] = { name = "Spelling" },
+            { "<leader>b", group = "Buffers" },
+            { "<leader>f", group = "Files" },
+            { "<leader>l", group = "LSP" },
+            { "<leader>m", group = "Misc" },
+            { "<leader>q", group = "Quickfix" },
+            { "<leader>R", group = "Replace" },
+            { "<leader>mS", group = "TreeSJ" },
+            { "<leader>s", group = "Search" },
+            { "<leader>t", group = "Toggles" },
+            { "<leader>z", group = "Spelling" },
+            { "<leader>w", group = "Windows" },
         },
     },
-    config = function (_, opts)
-        local wk = require("which-key")
-        wk.setup(opts)
-        wk.register(opts.groups)
-    end,
 }
