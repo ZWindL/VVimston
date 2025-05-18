9e1b1db0690178615ee2b098ad1912ad915d4f70
zwindl <zwindl@hotmail.com>
2025-05-18 17:41:33 +0800

    fix(lsp): fix jsonls executable name

--- lsp/jsonls.lua
+++ lsp/jsonls.lua
@@ -1,9 +1,12 @@
 return {
+    cmd = { 'vscode-json-language-server', '--stdio' },
+    filetypes = { 'json', 'jsonc' },
+    init_options = {
+        provideFormatter = true,
+    },
     -- use on_init hook to defer the schema load as third-party plugins are not loaded
-    on_init = function (client)
-        client.config.settings.json = vim.tbl_deep_extend("force", client.config.settings.json, {
-            schemas = require('schemastore').json.schemas(),
-            validate = { enable = true },
-        })
+    before_init = function(client)
+        -- client.config.settings.json = vim.tbl_deep_extend("force", client.config.settings.json, {
+        client.config.settings.json.schemas = require('schemastore').json.schemas()
     end
 }
--- lsp/yamlls.lua
+++ lsp/yamlls.lua
@@ -1,6 +1,6 @@
 return {
     -- use on_init hook to defer the schema load as third-party plugins are not loaded
-    on_init = function(client)
+    before_init = function(_, client)
         client.config.settings.yaml = vim.tbl_deep_extend("force", client.config.settings.yaml, {
             schemaStore = {
                 -- You must disable built-in schemaStore support if you want to use
