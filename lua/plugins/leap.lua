2ba148635ac0ab75104a00d0c7816c71e4afd28b
Winston Li <zefeng.li@rightcapital.com>
2025-12-25 09:58:03 +0800

    Improve leap mappings.

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -6,18 +6,21 @@ return {
         "ggandor/leap.nvim",
         dependencies = { "tpope/vim-repeat" },
         config = function()
-            -- The default keybindings are
-            -- vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
-            -- vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
-            -- Sneak-style
-            -- vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-            -- vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
-            -- vim.keymap.set('n', 'gs', '<Plug>(leap-from-window)')
             require('leap').setup({})
-            vim.keymap.set({ 'x', 'o' }, 'x', '<Plug>(leap-forward-till)')
-            vim.keymap.set({ 'x', 'o' }, 'X', '<Plug>(leap-backward-till)')
-            -- Incremental treesitter node selection
-            map({ 'n', 'x', 'o' }, 'gt', '<Plug>(leap-anywhere)', { desc = "Leap to everywhere" })
+            map({ 'n', 'x', 'o' }, '<C-\'>', '<Plug>(leap)')
+            map('n', 'S', '<Plug>(leap-from-window)')
+            -- Sneak-style
+            map({ 'n', 'x', 'o' }, '<C-;>', '<Plug>(leap-forward)')
+            map({ 'n', 'x', 'o' }, '<C-,>', '<Plug>(leap-backward)')
+            -- vim.keymap.set('n', 'gs', '<Plug>(leap-from-window)')
+            map({ 'x', 'o' }, 'x', '<Plug>(leap-forward-till)')
+            map({ 'x', 'o' }, 'X', '<Plug>(leap-backward-till)')
+            map({ 'n', 'x', 'o' }, '<C-/>', '<Plug>(leap-anywhere)', { desc = "Leap to anywhere" })
+            map({ 'x', 'o' }, 'R', function()
+                require('leap.treesitter').select {
+                    opts = require('leap.user').with_traversal_keys('R', 'r')
+                }
+            end)
         end,
     },
     {
@@ -25,6 +28,6 @@ return {
         dependencies = {
             "ggandor/leap.nvim"
         },
-        opts = true;
+        opts = true,
     },
 }
